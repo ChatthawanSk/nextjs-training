65227994d0fa1c4aca317a42c37c424a2e071889
Medivhh <chattawansakhon@gmail.com>
2024-05-03 16:46:20 +0700

    gif

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import rickroll from "../../public/rickroll-roll.gif";
 
 type Props = {};
 
 export default function page({}: Props) {
   return (
     <>
-      <h1>Name : Chatthawan Sakhon</h1>
-      <h1>Email : chatthawan_s@protossgroup.com</h1>
-      <h1>Tel : 0955355447</h1>
+      <div className="container">
+        <Image
+          src={rickroll}
+          alt="never gonna give you up"
+          height={500}
+          width={500}
+        />
+        <h1>Name : Chatthawan Sakhon</h1>
+        <h1>Email : chatthawan_s@protossgroup.com</h1>
+        <h1>Tel : 0955355447</h1>
+      </div>
     </>
   );
 }
